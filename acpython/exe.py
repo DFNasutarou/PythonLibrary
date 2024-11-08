d111e8086c9c66af9515c136c5a24515e4438a2e
Ext_DIT_MOTONAGA Shunsuke <motonaga.shunsuke@ditgroup.jp>
2024-11-08 18:42:00 +0900

    Tree64

--- acpython/exe.py
+++ acpython/exe.py
@@ -1,89 +1,112 @@
-# 連結リスト add/insertとremoveをO(1)
-class DoubleLinkedList():
-  class Node():
-    def __init__(s, data, previous=None, next=None):
-      s.data = data
-      s.previous = previous
-      s.next = next
-
+class Tree64:
+  HEIGHT = 8
+  BIT_SIZE = 6
+  SIZE = 2 << BIT_SIZE
+  MASK = SIZE - 1
+  class Node:
+    def __init__(s):
+      s.D = 0
+      s.L = [None] * Tree64.SIZE
+    def add(s, n, height):
+      if height == -1 : return
+      shift = Tree64.BIT_SIZE * height
+      x = (n >> shift) & Tree64.MASK
+      if s.L[x] == None: s.L[x] = Tree64.Node()
+      s.L[x].add(n, height - 1)
+      s.D |= (1 << x)
+    def erace(s, n, height):
+      if height == -1: return True
+      shift = Tree64.BIT_SIZE * height
+      x = (n >> shift) & Tree64.MASK
+      if s.L[x] == None: return False
+      if s.L[x].erace(n, height - 1):
+        s.L[x] = None
+        s.D ^= (1 << x)
+        if s.D == 0: return True
+      return False
+    def find(s, n, height):
+      if height == -1: return True
+      shift = Tree64.BIT_SIZE * height
+      x = (n >> shift) & Tree64.MASK
+      if s.L[x] == None: return False
+      return s.L[x].find(n, height - 1)
+    def find_upper(s, n, height):
+      if height == -1: return 0
+      shift = Tree64.BIT_SIZE * height
+      x = (n >> shift) & Tree64.MASK
+      if s.L[x] != None:
+        ans = s.L[x].find_upper(n, height - 1)
+        if ans != None: return (x << shift) + ans
+      d = s.D & ~((1 << (x + 1)) - 1)
+      if d == 0: return None
+      y = (d & -d).bit_length() - 1
+      if y == -1: return None
+      return (x << shift) + s.L[y].find_min(height - 1)
+    def find_lower(s, n, height):
+      if height == -1: return 0
+      shift = Tree64.BIT_SIZE * height
+      x = (n >> shift) & Tree64.MASK
+      if s.L[x] != None:
+        ans = s.L[x].find_lower(n, height - 1)
+        if ans != None: return (x << shift) + ans
+      d = s.D & (1 << x) - 1
+      if d == 0: return None
+      y = d.bit_length() - 1
+      if y == -1: return None
+      return (x << shift) + s.L[y].find_max(height - 1)
+    def find_min(s, height):
+      if height == -1: return 0
+      if s.D == 0: return None
+      x = (s.D & -s.D).bit_length() - 1
+      if height == 0: return x
+      shift = Tree64.BIT_SIZE * height
+      return (x << shift) + s.L[x].find_min(height - 1)
+    def find_max(s, height):
+      if height == -1: return 0
+      if s.D == 0: return None
+      x = s.D.bit_length() - 1
+      if height == 0: return x
+      shift = Tree64.BIT_SIZE * height
+      return (x << shift) + s.L[x].find_max(height - 1)
+    
   def __init__(s):
-    s._head = s.Node('DUMMY-HEAD')
-    s._tail = s.Node('DUMMY-TAIL', previous=s._head)
-    s._head.next = s._tail
-    s.length = 0
-    s.Nodes = {}
+    s.root = Tree64.Node()
+  def add(s, n):
+    s.root.add(n, Tree64.HEIGHT - 1)
+  def erace(s, n):
+    s.root.erace(n, Tree64.HEIGHT - 1)
+  def find(s, n):
+    return s.root.find(n, Tree64.HEIGHT - 1)
+  def find_upper(s, n):
+    ans =  s.root.find_upper(n, Tree64.HEIGHT - 1)
+    if ans == None: return -1
+    return ans
+  def find_lower(s, n):
+    ans = s.root.find_lower(n, Tree64.HEIGHT - 1)
+    if ans == None: return -1
+    return ans
+  
+N, Q = list(map(int, input().split()))
+T = list(input())
+ck = [list(map(int, input().split())) for _ in range(Q)]
 
-  # リストを設定
-  def setList(s, data):
-    for a in data:
-      s.add(a)
+tree = Tree64()
+for i in range(N):
+  if T[i] == "1":
+    tree.add(i)
 
-  # データを末尾に連結O(1)
-  def add(s, data):
-    node = s.Node(data, previous=s._tail.previous, next=s._tail)
-    s._tail.previous.next = node
-    s._tail.previous = node
-    s.length += 1
-    s.Nodes[data] = node
-
-  # データを削除O(1)
-  def remove(s, data):
-    target = s.Nodes[data]
-    target.previous.next = target.next
-    target.next.previous = target.previous
-    del target
-    s.length -= 1
-
-  # data_iの後にdataを追加 O(1)
-  def insert(s, deta_i, data):
-    target = s.Nodes[deta_i]
-    node = s.Node(data, previous=target, next=target.next)
-    target.next.previous = node
-    target.next = node
-    s.length += 1
-    s.Nodes[data] = node
-
-  # リスト取得
-  def getList(s):
-    return s.list
-       
-  def pophead(s):
-    target = s._head.next
-    if target == s._tail:
-      return None
-    s._head.next = target.next
-    target.next.previous = s._head
-    data = target.data
-    s.length -= 1
-    del target
-    return data
-
-  def poptail(s):
-    target = s._tail.previous
-    if target == s._head:
-      return None
-    s._tail.previous = target.previous
-    target.previous.next = s._tail
-    data = target.data
-    s.length -= 1
-    del target
-    return data
-
-  def print_reverse(s):
-    node = s._tail.previous
-    while node != s._head:
-      print(node.drata)
-      node = node.pevious
-
-  @property
-  def list(s):
-    node_data = []
-    node = s._head.next
-    while node != s._tail:
-      node_data.append(node.data)
-      node = node.next
-    return node_data
-
-  def __iter__(s):
-    node_data = s.list
-    return iter(node_data)
+for c, k in ck:
+  match c:
+    case 0:
+      tree.add(k)
+    case 1:
+      tree.erace(k)
+    case 2:
+      if tree.find(k):
+        print(1)
+      else:
+        print(0)
+    case 3:
+      print(tree.find_upper(k))
+    case 4:
+      print(tree.find_lower(k))
